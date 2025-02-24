17277ed719810a7733e4b38c70c9478ff5d11816
alemalagni <alemalagni@gmail.com>
2025-02-24 17:15:12 +0100

    fix: script

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const km = prompt('Inserisci i km da percorrere:');
 totale = km * tariffa
 const età = prompt('Inserisci la tua èta:');
 if ( età < 18 ) {
-    totale = totale * 0.20
+    totale = totale * 0.80
 } else if ( età > 65 ) {
-    totale = totale * 0.40
+    totale = totale * 0.60
 }
-console.log(totale)
+console.log('Il totale da pagare è di '+Math.round(totale * 100) / 100+'€')
